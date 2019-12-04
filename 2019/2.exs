90f75d996130360bd609f4b0683d3c70c5567c57
Filipe Cabaço <filipecabaco@gmail.com>
2019-12-04 17:33:28 +0000

    Mid commit with issues still...

--- 2019/2.exs
+++ 2019/2.exs
@@ -1,30 +1,60 @@
 defmodule Alarm do
-  def process(ops), do: process_opcode(Enum.slice(ops, 0, 4), ops, 0)
+  def process(ops, noun, verb) do
+    ops =
+      ops
+      |> List.update_at(1, fn _ -> noun end)
+      |> List.update_at(2, fn _ -> verb end)
 
-  defp process_opcode([], acc, _), do: acc
-
-  defp process_opcode([1 | _] = ops, acc, pos) do
-    res = sum(ops, acc)
-    process_opcode(Enum.slice(res, pos, 4), res, pos + 4)
+    try do
+      process_opcode(Enum.slice(ops, 0, 4), ops, 0)
+    rescue
+      _ -> nil
+    end
   end
 
-  defp process_opcode([2 | _] = ops, acc, pos) do
-    res = mult(ops, acc)
-    process_opcode(Enum.slice(res, pos, 4), res, pos + 4)
-  end
+  defp process_opcode(ops, [memory | _], _) when length(ops) < 4, do: memory
 
   defp process_opcode([99 | _], acc, _), do: process_opcode([], acc, 0)
 
-  defp sum([_, i1, i2, t | _], acc),
-    do: List.update_at(acc, t, fn _ -> Enum.at(acc, i1) + Enum.at(acc, i2) end)
+  defp process_opcode([1, _, _, _] = ops, acc, pos),
+    do: (fn res -> process_opcode(Enum.slice(res, pos, 4), res, pos + 4) end).(op(ops, acc, &+/2))
 
-  defp mult([_, i1, i2, t | _], acc),
-    do: List.update_at(acc, t, fn _ -> Enum.at(acc, i1) * Enum.at(acc, i2) end)
+  defp process_opcode([2, _, _, _] = ops, acc, pos),
+    do: (fn res -> process_opcode(Enum.slice(res, pos, 4), res, pos + 4) end).(op(ops, acc, &*/2))
+
+  defp process_opcode(_, acc, _), do: process_opcode([], acc, 0)
+
+  defp op([_, i1, i2, t], acc, op) when i1 <= length(acc) and i2 <= length(acc) do
+    List.update_at(acc, t, fn _ -> op.(Enum.at(acc, i1), Enum.at(acc, i2)) end)
+  end
+
+  defp op(_, _, _), do: raise("Out of bounds")
+
+  def find_noun_verb(ops, expected, range) do
+    0..range
+    |> Stream.flat_map(fn noun -> Stream.map(0..range, fn verb -> {noun, verb} end) end)
+    |> Stream.map(&check_expected(&1, ops, expected))
+    |> Enum.reject(&is_nil/1)
+  end
+
+  defp check_expected({noun, verb}, ops, expected) do
+    ops
+    |> process(noun, verb)
+    |> (fn res ->
+          if res == expected do
+            {noun, verb}
+          end
+        end).()
+  end
 end
 
-File.read!("./input2_edited.txt")
-|> String.split(",")
-|> Enum.map(&String.to_integer/1)
-|> Alarm.process()
-|> Enum.at(0)
+content =
+  File.read!("./input")
+  |> String.split(",")
+  |> Enum.map(&String.to_integer/1)
+
+content
+|> Alarm.process(12, 2)
 |> IO.inspect(label: "Solution 1")
+
+IO.inspect(Alarm.find_noun_verb(content, 19_690_720, 99))
