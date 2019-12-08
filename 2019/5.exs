483f785b9c7b9e077c1fde26abc4740ebeba103b
filipecabaco <filipecabaco@gmail.com>
2019-12-08 15:38:37 +0000

    Problem 5 solution

---
+++ 2019/5.exs
@@ -0,0 +1,165 @@
+defmodule Alarm do
+  def process(ops), do: process_opcode(Enum.slice(ops, 0, 1), Enum.slice(ops, 1, 3), 0, ops)
+
+  defp process_opcode([], _, _, acc), do: acc
+
+  defp process_opcode([opcode], args, pos, acc) do
+    %{opcode: opcode, par_mode: par_mode} = decompose_opcode(opcode)
+    case opcode do
+      :halt ->
+        acc
+
+      _ ->
+        {pos, acc} = process(opcode, par_mode, args, pos, acc)
+        process_opcode(Enum.slice(acc, pos, 1), Enum.slice(acc, pos + 1, 3), pos, acc)
+    end
+  end
+
+  defp process(:sum, %{p1: p1, p2: p2}, [a1, a2, a3], pos, acc) do
+    a1 = process_param(p1, a1, acc)
+    a2 = process_param(p2, a2, acc)
+    {pos + steps(:sum), update(a3, fn _ -> a1 + a2 end, acc)}
+  end
+
+  defp process(:mult, %{p1: p1, p2: p2}, [a1, a2, a3], pos, acc) do
+    a1 = process_param(p1, a1, acc)
+    a2 = process_param(p2, a2, acc)
+    {pos + steps(:mult), update(a3, fn _ -> a1 * a2 end, acc)}
+  end
+
+  defp process(:input, _, [t | _], pos, acc),
+    do: {pos + steps(:input), update(t, &input/1, acc)}
+
+  defp process(:output, %{p1: p1}, [a1 | _], pos, acc)
+    do
+      IO.inspect(process_param(p1, a1, acc), label: "Output")
+      {pos + steps(:output), acc}
+    end
+
+  defp process(:jump_true, %{p1: p1, p2: p2}, [a1, a2 | _], pos, acc) do
+    a1 = process_param(p1, a1, acc)
+    a2 = process_param(p2, a2, acc)
+
+    case a1 != 0 do
+      true -> {a2, acc}
+      false -> {pos + steps(:jump_true), acc}
+    end
+  end
+
+  defp process(:jump_false, %{p1: p1, p2: p2}, [a1, a2 | _], pos, acc) do
+    a1 = process_param(p1, a1, acc)
+    a2 = process_param(p2, a2, acc)
+
+    case a1 == 0 do
+      true -> {a2, acc}
+      false -> {pos + steps(:jump_false), acc}
+    end
+  end
+
+  defp process(:less, %{p1: p1, p2: p2}, [a1, a2, a3], pos, acc) do
+    a1 = process_param(p1, a1, acc)
+    a2 = process_param(p2, a2, acc)
+
+    {pos + steps(:less),
+     update(
+       a3,
+       fn _ ->
+         case a1 < a2 do
+           true -> 1
+           false -> 0
+         end
+       end,
+       acc
+     )}
+  end
+
+  defp process(:equals, %{p1: p1, p2: p2}, [a1, a2, a3], pos, acc) do
+    a1 = process_param(p1, a1, acc)
+    a2 = process_param(p2, a2, acc)
+
+    {pos + steps(:equals),
+     update(
+       a3,
+       fn _ ->
+         case a1 == a2 do
+           true -> 1
+           false -> 0
+         end
+       end,
+       acc
+     )}
+  end
+
+  defp process_param(:position, i, acc), do: Enum.at(acc, i)
+  defp process_param(:immediate, i, _), do: i
+
+  defp update(t, op, acc), do: List.update_at(acc, t, op)
+
+  defp input(_) do
+    IO.puts("Input value please")
+    IO.read(:stdio, :line) |> String.trim() |> String.to_integer()
+  end
+
+  defp decompose_opcode(opcode, acc \\ [])
+
+  defp decompose_opcode(0, [e]),
+    do: %{
+      opcode: op("0#{e}"),
+      par_mode: %{p1: param_mode(0), p2: param_mode(0), p3: param_mode(0)}
+    }
+
+  defp decompose_opcode(0, [d, e]),
+    do: %{
+      opcode: op("#{d}#{e}"),
+      par_mode: %{p1: param_mode(0), p2: param_mode(0), p3: param_mode(0)}
+    }
+
+  defp decompose_opcode(0, [c, d, e]),
+    do: %{
+      opcode: op("#{d}#{e}"),
+      par_mode: %{p1: param_mode(c), p2: param_mode(0), p3: param_mode(0)}
+    }
+
+  defp decompose_opcode(0, [b, c, d, e]),
+    do: %{
+      opcode: op("#{d}#{e}"),
+      par_mode: %{p1: param_mode(c), p2: param_mode(b), p3: param_mode(0)}
+    }
+
+  defp decompose_opcode(0, [a, b, c, d, e]),
+    do: %{
+      opcode: op("#{d}#{e}"),
+      par_mode: %{p1: param_mode(c), p2: param_mode(b), p3: param_mode(a)}
+    }
+
+  defp decompose_opcode(opcode, acc),
+    do: decompose_opcode(trunc(opcode / 10), [rem(opcode, 10)] ++ acc)
+
+  defp op("99"), do: :halt
+  defp op("01"), do: :sum
+  defp op("02"), do: :mult
+  defp op("03"), do: :input
+  defp op("04"), do: :output
+  defp op("05"), do: :jump_true
+  defp op("06"), do: :jump_false
+  defp op("07"), do: :less
+  defp op("08"), do: :equals
+
+  defp param_mode(0), do: :position
+  defp param_mode(1), do: :immediate
+
+  defp steps(:halt), do: 0
+  defp steps(:sum), do: 4
+  defp steps(:mult), do: 4
+  defp steps(:input), do: 2
+  defp steps(:output), do: 2
+  defp steps(:jump_true), do: 3
+  defp steps(:jump_false), do: 3
+  defp steps(:less), do: 4
+  defp steps(:equals), do: 4
+end
+
+File.read!("./input5")
+|> String.split(",")
+|> Enum.map(&String.to_integer/1)
+|> Alarm.process()
